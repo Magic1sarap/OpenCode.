226ecbd72e652bc2f1fb2bb93bc0dcca924a2019
Isaac <isaacagatep@gmail.com>
2020-04-23 22:29:05 +0800

    latest

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+
+  
+  constructor() { 
+
+  
+  }
 
   ngOnInit(): void {
+
+
+
   }
+  
 
 }
